219eae7fbd5738b76adf79b4ec110ddb92b51f65
Kiet1906 <nguyenanhkiet190605@gmail.com>
2026-04-24 13:54:09 +0700

    LAN THU 1

---
+++ THIGK2/src/main/java/thigk2/nguyenanhkiet/Thigk2Application.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenanhkiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thigk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thigk2Application.class, args);
+	}
+
+}
---
+++ THIGK2/src/test/java/thigk2/nguyenanhkiet/Thigk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenanhkiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thigk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
